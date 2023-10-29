535fefce8f9b1c9812ef4af306b7b7f32fbc1558
Kushagradalal1 <kushagradalal33@gmail.com>
2023-10-29 19:16:22 +0530

    collection framework

---
+++ src/ArrayLIst/LearnArrayList.java
@@ -0,0 +1,19 @@
+package ArrayLIst;
+
+import java.util.ArrayList;
+
+public class LearnArrayList {
+    public static void main(String[] args) {
+        //dynamic array we have an arraylist :
+        //add() : it will add the element at the last position.
+        //add(int index, int element) : it will at the element to the specific position add.
+
+        ArrayList<String> studentsName=new ArrayList<>();
+//        studentsName.add("A");
+//        studentsName.add("B");
+//        studentsName.add("C");
+//        studentsName.add("D");
+        System.out.println(studentsName);
+
+    }
+}
